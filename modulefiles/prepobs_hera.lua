35e4b2de1d36f9a5e81fa2f6b92f0c9d83683171
Kyle Gerheiser <Kyle.Gerheiser@noaa.gov>
2022-02-02 17:15:24 +0000

    Find and link to MPI for oiqcbufr

--- modulefiles/prepobs_hera.lua
+++ modulefiles/prepobs_hera.lua
@@ -7,6 +7,7 @@ load("cmake/3.20.1")
 prepend_path("MODULEPATH", "/scratch2/NCEPDEV/nwprod/hpc-stack/libs/hpc-stack/modulefiles/stack")
 load("hpc/1.1.0")
 load("hpc-intel/18.0.5.274")
+load("hpc-impi/2018.0.4")
 
 -- Load common modules for this package
 load("prepobs_common")
--- modulefiles/prepobs_orion.lua
+++ modulefiles/prepobs_orion.lua
@@ -7,6 +7,7 @@ load("cmake/3.18.1")
 prepend_path("MODULEPATH", "/apps/contrib/NCEP/libs/hpc-stack/modulefiles/stack")
 load("hpc/1.1.0")
 load("hpc-intel/2018.4")
+load("hpc-impi/2018.4")
 
 -- Load common modules for this package
 load("prepobs_common")
