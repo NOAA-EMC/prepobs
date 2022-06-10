5b599ceecefe6a84c8b74168415d292f2baa924c
Shelley Melchior <Shelley.Melchior@noaa.gov>
2022-06-10 00:15:03 +0000

    Including NCO changes for consideration to be included in release/prepobs.v1.0.0.

--- ush/gdas_summary.sh
+++ ush/gdas_summary.sh
@@ -198,7 +198,15 @@ cat $FIXprepobs/gdascounts_html  >> ${MONTH}.htmlscript
 # Move this over to the public web server.
 #
 if [ "$SENDWEB" = 'YES' ]; then
-  if [ "$USER" = nwprod ]; then
+  if [ "$USER" = ops.prod ]; then
+    if [ $PARATEST = YES ]; then
+    scp ${Month}_${YEAR}.html nwprod@ncorzdm:/home/people/nco/nwprod/pmb/nwtest/gdas/
+    scp index.shtml nwprod@ncorzdm:/home/people/nco/nwprod/pmb/nwtest/gdas/
+    else
+    scp ${Month}_${YEAR}.html nwprod@ncorzdm:/home/people/nco/nwprod/pmb/nw${envir}/gdas/
+    scp index.shtml nwprod@ncorzdm:/home/people/nco/nwprod/pmb/nw${envir}/gdas/
+    fi
+  elif [ $USER = ops.para ]; then
     scp ${Month}_${YEAR}.html nwprod@ncorzdm:/home/people/nco/nwprod/pmb/nw${envir}/gdas/
     scp index.shtml nwprod@ncorzdm:/home/people/nco/nwprod/pmb/nw${envir}/gdas/
   else # developer
