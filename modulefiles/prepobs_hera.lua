a9e9b4f9fa3a1542abdd671820d9a80ee31778f5
Rahul Mahajan <aerorahul@users.noreply.github.com>
2024-03-04 14:15:40 -0500

    rocky8 update MODULEPATH for hera

--- modulefiles/prepobs_hera.lua
+++ modulefiles/prepobs_hera.lua
@@ -2,7 +2,9 @@ help([[
 Load environment to build prepobs on Hera
 ]])
 
-prepend_path("MODULEPATH", "/scratch1/NCEPDEV/nems/role.epic/spack-stack/spack-stack-1.6.0/envs/gsi-addon-dev/install/modulefiles/Core")
+--prepend_path("MODULEPATH", "/scratch1/NCEPDEV/nems/role.epic/spack-stack/spack-stack-1.6.0/envs/gsi-addon-dev/install/modulefiles/Core")
+prepend_path("MODULEPATH", "/scratch1/NCEPDEV/nems/role.epic/spack-stack/spack-stack-1.6.0/envs/gsi-addon-dev-rocky8/install/modulefiles/Core")
+
 
 stack_intel_ver=os.getenv("stack_intel_ver") or "2021.5.0"
 stack_impi_ver=os.getenv("stack_impi_ver") or "2021.5.1"
