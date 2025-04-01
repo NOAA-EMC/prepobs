5e261db3ea4aa118c9c8265d3345513ed3f60720
Taylor Roper <taylor.roper@noaa.gov>
2025-04-01 18:39:11 +0000

    adding ability to install on NOAA cloud (only tested on AWS)

---
+++ modulefiles/prepobs_noaacloud.lua
@@ -0,0 +1,20 @@
+help([[
+Load environment to build prepobs on NOAA cloud
+]])
+
+prepend_path("MODULEPATH", os.getenv("spack_stack_mod_path"))
+
+stack_intel_ver=os.getenv("stack_intel_ver") or "2021.10.0"
+stack_impi_ver=os.getenv("stack_impi_ver") or "2021.10.0"
+cmake_ver=os.getenv("cmake_ver") or "3.23.1"
+
+load("gnu")
+load(pathJoin("stack-intel", stack_intel_ver))
+load(pathJoin("stack-intel-oneapi-mpi", stack_impi_ver))
+load(pathJoin("cmake", cmake_ver))
+--unload("gnu")
+
+-- Load common modules for this package
+load("prepobs_common")
+
+whatis("Description: prepobs build environment")
