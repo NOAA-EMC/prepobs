a467c7b055f4a70143bc66e1f489b2f5822b76f7
David Huber <david.huber@noaa.gov>
2025-10-09 15:14:47 +0000

    Update Hera to spack-stack 1.9.2

--- modulefiles/prepobs_hera.lua
+++ modulefiles/prepobs_hera.lua
@@ -6,7 +6,7 @@ prepend_path("MODULEPATH", os.getenv("spack_stack_mod_path"))
 
 stack_oneapi_ver=os.getenv("stack_oneapi_ver") or "2024.2.1"
 stack_intel_oneapi_mpi_ver=os.getenv("stack_intel_oneapi_mpi_ver") or "2021.13"
-cmake_ver=os.getenv("cmake_ver") or "3.30.2"
+cmake_ver=os.getenv("cmake_ver") or "3.27.9"
 
 load(pathJoin("stack-oneapi", stack_oneapi_ver))
 load(pathJoin("stack-intel-oneapi-mpi", stack_intel_oneapi_mpi_ver))
